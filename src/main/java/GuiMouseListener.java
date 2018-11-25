ead9558eed4ad0ce425769e9d76c9e8c6b90a8ea
Joshua Christl <joshua.christl@tum.de>
2018-11-25 15:38:12 +0100

    Added minimal gui including MouseListener

--- src/main/java/GuiMouseListener.java
+++ src/main/java/GuiMouseListener.java
@@ -1,5 +1,56 @@
-package PACKAGE_NAME;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class GuiMouseListener {
+public class GuiMouseListener implements MouseListener {
 
+  /**
+   * The button ID ([0;31])
+   */
+  private int id;
+
+  /**
+   * The Gui, which the callback is sent to (via Gui.buttonCallback(int id))
+   */
+  private Gui gui;
+
+  /**
+   * The constructor of a MouseListener.
+   * Gets called by every button
+   * @param gui see gui attribute
+   * @param id see id attribute
+   */
+  public GuiMouseListener(Gui gui, int id) {
+    this.id = id;
+    this.gui = gui;
+  }
+
+  /**
+   * Called automatically when the mouse clicks on a button.
+   * Invoked gui.buttonCallback(id)
+   * @param e
+   */
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    gui.buttonCallback(id);
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+
+  }
 }
--- src/main/java/Match.java
+++ src/main/java/Match.java
@@ -1,19 +1,29 @@
 public class Match {
 
+    private Gui gui;
     private PlayingField playingField;
+
+    public Match() {
+        // TODO maybe extract this 32 and make it a constant or sth
+        //      (also in other classes)
+        this.gui = new Gui(32);
+    }
 
     public void initMatch() {
         playingField = new PlayingField();
+        gui.update(playingField);
     }
 
     public void makeMove(int hole) {
         if (!isGameFinished()) {
             playingField.makeMove(hole);
         }
+
+        // TODO does this belong here?
+        gui.update(playingField);
     }
 
     public boolean isGameFinished() {
-
         for (int i = 0; i < 32; i++)
         {
             if (playingField.getNumberOfStones(i) > 1) {
